9f4b5f20227605e677770b82cb863dd55cdd92ec
Gal Efraty <gal.efraty@gmail.com>
2019-10-23 16:03:01 +0300

    finished auth flow

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+const keys = require("./config/keys");
+require("./models/User");
+require("./services/passport");
 
 const PORT = process.env.PORT || 5000;
 const app = express();
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000, //cookie expmire in 30 days
+    keys: [keys.cookieKey] //to encrypt the cookie
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.get("/", (req, res) => {
-  res.send({ hi: "there" });
-});
+require("./routes/authRoutes")(app);
+
+mongoose
+  .connect(keys.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log("Connected to MongoDB!");
+  })
+  .catch(err => console.log("Error on start: " + err.stack));
 
 app.listen(PORT);
