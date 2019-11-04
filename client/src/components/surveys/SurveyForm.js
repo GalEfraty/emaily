8df41c96221a8b40a3f5f9e59df719aab9f99ec4
Gal Efraty <gal.efraty@gmail.com>
2019-11-04 11:56:15 +0200

    first commit to github emaily

---
+++ client/src/components/surveys/SurveyForm.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import { reduxForm, Field } from "redux-form";
+
+//SurveyForm shows a form to the user to add input
+class SurveyForm extends Component {
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
+          <Field type="text" name="surveyTitle" componenet="input" />
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default reduxForm({
+  form: "surveyForm"
+})(SurveyForm);
